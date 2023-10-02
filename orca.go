86848971e130e7a4af3b09b31d5b47e951bb3f23
Nguyen Thanh Quang <teddyowo@outlook.com>
2023-10-02 13:19:58 +0700

    :tada: initial commit

---
+++ orca.go
@@ -0,0 +1,34 @@
+package orca
+
+import (
+	"blizzard/config"
+	"blizzard/logger"
+	"context"
+	"github.com/bwmarrin/discordgo"
+	"strings"
+)
+
+type Orca struct {
+	*discordgo.Session
+}
+
+var Instance *Orca
+
+func Init(ctx context.Context) {
+	conf := config.Config.Discord
+	if conf == nil || strings.TrimSpace(conf.Token) == "" {
+		return
+	}
+	logger.Orca.Info().Msg("initializing orca")
+	s, e := discordgo.New("Bot " + conf.Token)
+	logger.Panic(e, "failed to create a Discord session")
+	go func() {
+		<-ctx.Done()
+		s.Close()
+	}()
+	Instance = &Orca{s}
+	s.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
+		logger.Orca.Info().Msg("orca is ready")
+	})
+	logger.Panic(s.Open(), "failed to open a Discord session")
+}
